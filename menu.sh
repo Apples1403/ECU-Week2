b44d2f1e9ed9fefc257e85ff20622ecbbc753471
Ubuntu <colin_appleby@hotmail.com>
2021-07-25 10:21:56 +0000

    Commented and fully tested

--- menu.sh
+++ menu.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+# get the user to enter his password
+
 ./passwordcheck.sh
+
+#if he got it right - continue
+
 if (test $? == 0) ; then
+
+   # set initial value for option of 0
+
    option=0
+
+# repeat the loop until user enters value in the range of 1-3
+
    until ((option >= 1 && option <= 3))
    do
       echo "Select an Option in the range 1-3"
@@ -11,18 +23,30 @@ if (test $? == 0) ; then
       echo -n "Please enter Option: "
       read option
       if ((option >= 1 && option <= 3)); then
-        echo " "
+        
+         # display blank line
+
+         echo " "
       else
+
+         # let the user know his value was out of range
+
          echo " "
          echo "Please enter a Number in the Range 1 to 3"
       fi
    done    
+
+   # based on input value run the appropriate script
+ 
    case $option in
         1) ./foldermaker.sh;;
         2) ./foldercopier.sh;;
         3) ./setPassword.sh;;
    esac
-else
+else   
+
+   # user got password wrong - exit
+
    echo "Goodbye"
    exit 0
 fi
